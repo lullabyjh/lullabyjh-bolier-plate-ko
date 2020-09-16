b152a1828ddfec55eb524ebccf4806340c3ad148
lullabyjh <heroup@hanmail.net>
2020-09-16 14:52:55 +0900

    비밀정보 추가

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-const express = require('express')
-const app = express()
-const port = 5000
+const express = require('express');
+const app = express();
+const port = 5000;
+const bodyParser = require('body-parser');
+const {User} = require("./models/User");
+
+
+//applicaiton/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://lee:1234@boilerplate.ubk0v.mongodb.net/test?retryWrites=true&w=majority', {
@@ -8,7 +16,23 @@ mongoose.connect('mongodb+srv://lee:1234@boilerplate.ubk0v.mongodb.net/test?retr
 }).then(()=> console.log('MongoDB Connected...')).catch(err => console.log(err))
 
 app.get('/', (req, res) => {
-  res.send('Hello World! ~ 안녕하세요.')
+  res.send('Hello World! ~ 안녕하세요. ㅎㅎㅎ')
+})
+
+app.post('/register', (req, res) => {
+
+  //회원 가입 할때 필요한 정보들을 Client에서 가져오면
+  //그것들을 데이터 베이스에 넣어준다.
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({success:false, err})
+    return res.status(200).json({
+      success:true
+    })
+  })
+
 })
 
 app.listen(port, () => {
